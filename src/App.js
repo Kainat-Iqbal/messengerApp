267b6dc170c02f0e6f257eb8e58a87ed8578d329
Kainat-Iqbal <kainatjiq@gmail.com>
2023-11-19 18:03:14 +0500

    second task done

---
+++ src/App.js
@@ -0,0 +1,204 @@
+import { useEffect, useRef, useState } from "react";
+import { Box , Container , VStack , HStack , Button, Input, Text } from "@chakra-ui/react";
+import Message from "./components/Message";
+import {onAuthStateChanged , getAuth , GoogleAuthProvider , signInWithPopup , signOut } from "firebase/auth"
+import {getFirestore , addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from "firebase/firestore"
+import { app } from "./components/firebase";
+import { ToastContainer, toast } from 'react-toastify';
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const loginHandler=async()=>{
+  const provider = new GoogleAuthProvider();
+
+  await signInWithPopup(auth, provider)
+  toast.success('Login Successful', {
+    position: "top-center",
+    autoClose: 500,
+    hideProgressBar: true,
+    theme: "light",
+    });
+  
+}
+
+
+function App() {
+
+  let[visible, setVisible] = useState(false);
+  
+  const [user , setUser] = useState(false)
+  const [message , setMessage] = useState('')
+  const [room , setRoom] = useState('OpenRoom')
+  const [temp , setTemp] = useState('OpenRoom')
+  const [messages , setMessages] = useState([])
+  
+  const logoutHandler=()=>{
+    setTemp('OpenRoom')
+    signOut(auth)
+    toast.warning('Logout!', {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: true,
+      theme: "dark",
+      });
+  }
+
+  const divForScroll = useRef(null)
+
+  const submithandler = async(e)=>{ 
+    e.preventDefault()
+    // console.log(user.displayName)
+  
+    try {
+      setMessage('')
+      await addDoc(collection(db,`${room}`),{
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        name: user.displayName,
+        createdAt: serverTimestamp(),
+
+      })
+
+      toast.success('sent', {
+        position: "top-center",
+        autoClose: 500,
+        hideProgressBar: true,
+        theme: "dark",
+        });
+      divForScroll.current.scrollIntoView({behavior: "smooth"})
+      
+    } catch (error) {
+      toast.error(error, {
+        position: "top-center",
+        autoClose: 500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+    }
+  
+  }
+
+  useEffect(()=>{
+    const q = query(collection(db,`${room}`),orderBy('createdAt',"asc"))
+
+    //concept of react life-cycle
+    const unsubscribe = onAuthStateChanged(auth,(data)=>{ 
+      // console.log(data)   
+      setUser(data)
+    })
+
+    const unsubscribeForMessage= onSnapshot(q,(snap)=>{
+
+      setMessages(
+        snap.docs.map((item)=>{
+          const id = item.id
+          return {id, ...item.data()}
+        })
+      )
+    })  
+
+    return ()=>{
+      unsubscribe()
+      unsubscribeForMessage()   
+    }  
+  },[room])
+
+  return (
+    <Box bg={'black.50'}>
+
+      <ToastContainer></ToastContainer>
+
+    {
+      user ? 
+      (
+        
+            <Container bgSize={'100% 100%'} bgImage={'https://img.freepik.com/free-vector/colorful-3d-background_23-2148484152.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697846400&semt=ais'} h={'100vh'} padding={'2'} > 
+            <VStack h={'full'}>
+
+              <HStack bgColor={'rgb(0, 0, 0, .5)'} w={'full'} justifyContent={"space-between"}>
+              <Button onClick={logoutHandler} colorScheme="red" >Logout</Button>
+              <Text color={'whiteAlpha.900'} fontWeight={'bold'} fontSize={'large'} textAlign={'center'} w={'full'}>{room}</Text>
+              </HStack>
+
+              <VStack gap={'0'} h={'full'} w={'full'} overflowY={'auto'} css={{'&::-webkit-scrollbar':{
+                display:'none',
+              }}}>
+                  {
+                    messages.map((item) => {
+                      return <Message
+                      key={item.id} //related to map method
+                      user={item.uid===user.uid ? 'me' : 'other'}
+                      msg={item.text}
+                      uri={item.uri}
+                      name={item.name}
+                      />
+                    })
+                  }
+                  <div ref={divForScroll}></div>
+              </VStack>
+
+              <form onSubmit={submithandler} style={{width:'100%'}} >
+              <HStack >
+                <Input
+                  value={message}
+                  onChange={(e)=>setMessage(e.target.value)} 
+                  placeholder="Enter a Message..." colorScheme="black" bg={'black.100'}></Input>
+                  <Button colorScheme="green" onClick={()=>setVisible(!visible)}>:)</Button>
+                  {visible && 
+                    <div style={{position:'absolute',top:'3cm', left:'20cm'}}>
+                    <Picker data={data} onEmojiSelect={(object)=>{
+                    setMessage(message+object.native)
+                    setVisible(!visible)
+                  }} />
+                    </div>
+                  }
+                <Button type="submit" colorScheme="green">Send</Button>
+              </HStack>
+      
+              </form>
+      
+            </VStack>
+            </Container>
+      
+      ) : (
+        <VStack h={'100vh'} justifyContent={'center'}>
+
+            <Text fontSize={'large'}>JOIN OR CREATE ROOM:</Text>
+            <HStack>
+
+            <Input value={temp} w={'60vh'} placeholder="Enter Room name..." bgColor={'blue.100'} onChange={(e)=>setTemp(e.target.value)}></Input>
+            <Button colorScheme="red" onClick={()=>setVisible(!visible)}>:)</Button>
+            {visible && 
+      <div style={{position:'absolute',top:'3cm', left:'20cm'}}>
+        <Picker data={data} onEmojiSelect={(object)=>{
+      setTemp(temp+object.native)
+      setVisible(!visible)
+    }} />
+      </div>
+    }
+            </HStack>
+            <Button onClick={
+              ()=>{setRoom(temp)
+                    loginHandler()
+                  }
+            } colorScheme="green">Sign In with Google</Button>
+        </VStack>
+  
+      )
+    }
+    </Box>
+
+  )
+}
+
+export default App;
